5a7f68be6a05f350b774a49265bd6722d9404b51
Felipe Mendes <luizfelipe.m93@gmail.com>
2023-07-15 22:11:56 -0300

    env variables test

--- counter.js
+++ counter.js
@@ -1,9 +1,13 @@
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+  // let counter = 0
+  // const setCounter = (count) => {
+  //   counter = count
+  //   element.innerHTML = `count is ${counter}`
+  // }
+  // element.addEventListener('click', () => setCounter(counter + 1))
+  // setCounter(0)
+  // process.env = { ...process.env, ...loadEnv(mode, process.cwd())};
+  const id = `${__id__}`;
+  element.innerHTML =  `id is ${id}`
+  console.log(id);
 }
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
-export default {
-  base: "/testing-vite-deploy"
-}
+import { defineConfig } from 'vite';
+import dotenv from 'dotenv';
+
+dotenv.config(); // load env vars from .env
+
+export default defineConfig({
+  define: {
+    __id__: `"${process.env.ID}"`,
+  },
+  base: "/testing-vite-deploy",
+});
